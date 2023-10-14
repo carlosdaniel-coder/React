40ae0ed1bfe6139ff9b57d0490c73b66b631f480
carlosdaniel-coder <cangastisticas@gmail.com>
2023-10-13 22:53:25 -0300

    estilização home

--- 02-ignite-timer/src/Router.tsx
+++ 02-ignite-timer/src/Router.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import { Home } from './pages/Home'
+import { Home } from './pages/Home/index'
 import { History } from './pages/History'
 import { DefaultLayout } from './layouts/DefaultLayout/index'
 
