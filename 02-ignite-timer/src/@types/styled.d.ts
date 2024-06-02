4ba3bc1a4c5b1e4a86037c09413a63ef18e0f2d5
Carlos Daniel <136939779+carlosdaniel-coder@users.noreply.github.com>
2024-06-02 20:22:03 -0300

    Delete 02-ignite-timer directory
    aconteceu imprevisto ai parei mais agr continuarei

--- 02-ignite-timer/src/@types/styled.d.ts
+++
@@ -1,7 +0,0 @@
-import { defaultTheme } from '../styled/themes/default'
-
-type ThemeType = typeof defaultTheme
-
-declare module 'styled-components' {
-  export interface DefaultTheme extends ThemeType {}
-}
