3285a6f7450b2b67e495e1d6ebf89b86d1f10b3b
Carlos Daniel <136939779+carlosdaniel-coder@users.noreply.github.com>
2024-08-06 17:43:01 -0300

    pagina signup toast tmb

--- pizzashop-web/src/App.tsx
+++ pizzashop-web/src/App.tsx
@@ -2,12 +2,14 @@ import './global.css'
 
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
 import { router } from './routes'
 export function App() {
   return (
     <HelmetProvider>
       <Helmet titleTemplate="%s | pizza.shop" />
+      <Toaster richColors />
       <RouterProvider router={router} />
     </HelmetProvider>
   )
--- pizzashop-web/src/pages/auth/sign-in.tsx
+++ pizzashop-web/src/pages/auth/sign-in.tsx
@@ -1,14 +1,50 @@
 import { Label } from '@radix-ui/react-label'
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import { toast } from 'sonner'
+import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 
+const signInForm = z.object({
+  email: z.string().email(),
+})
+
+type SignInForm = z.infer<typeof signInForm>
+
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>()
+
+  async function handleSignIn(data: SignInForm) {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+
+      toast.success('Enviamos um link para seu e-email.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignIn(data),
+        },
+      })
+    } catch {
+      toast.error('Credenciais inválidas!')
+    }
+  }
+
   return (
     <>
       <Helmet title="login" />
       <div className="p-8">
+        <Button asChild variant={'ghost'}>
+          <Link to="/sign-up" className="absolute right-8 top-8">
+            Novo estabelecimento
+          </Link>
+        </Button>
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
@@ -18,13 +54,13 @@ export function SignIn() {
               Aconpanhe suas vendas pelo painel de controle!
             </p>
           </div>
-          <form className="space-y-4">
+          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" type="email" />
+              <Input id="email" type="email" {...register('email')} />
             </div>
 
-            <Button className="w-full" type="submit">
+            <Button disabled={isSubmitting} className="w-full" type="submit">
               Acessar painel
             </Button>
           </form>
