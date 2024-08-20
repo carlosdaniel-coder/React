955b65d0be5d259aeda5305fa118328107ff3bc2
Carlos Daniel <136939779+carlosdaniel-coder@users.noreply.github.com>
2024-08-20 17:21:42 -0300

    cansera

---
+++ pizzashop-web/src/pages/app/dashboard/revenue-chart.tsx
@@ -0,0 +1,69 @@
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts'
+import colors from 'tailwindcss/colors'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+const data = [
+  { data: '10/12', revenu: 1200 },
+  { data: '11/12', revenu: 800 },
+  { data: '12/12', revenu: 900 },
+  { data: '13/12', revenu: 500 },
+  { data: '14/12', revenu: 200 },
+  { data: '15/12', revenu: 2000 },
+  { data: '16/12', revenu: 900 },
+]
+
+export function RevenueChart() {
+  return (
+    <Card className="col-span-6">
+      <CardHeader className="flex-row items-center justify-between pb-8">
+        <div className="space-y-1">
+          <CardTitle className="text-base font-medium">
+            Receita no periodo
+          </CardTitle>
+          <CardDescription>Receita diária no período</CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <ResponsiveContainer className="100%" height={240}>
+          <LineChart data={data} style={{ fontSize: 12 }}>
+            <XAxis dataKey="data" tickLine={false} axisLine={false} dy={16} />
+
+            <YAxis
+              stroke="#888"
+              axisLine={false}
+              tickLine={false}
+              width={80}
+              tickFormatter={(value: number) =>
+                value.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                })
+              }
+            />
+            <Line
+              type="linear"
+              strokeWidth={2}
+              dataKey="revenu"
+              stroke={colors.violet['500']}
+            ></Line>
+          </LineChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  )
+}
--- pizzashop-web/src/routes.tsx
+++ pizzashop-web/src/routes.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
-import { Deashboard } from './pages/app/deshboard'
+import { Deashboard } from './pages/app/dashboard/deshboard'
 import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
